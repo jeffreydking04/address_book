b2d1d18675c2a322b3acd899b601e78b1e518af6
Jeffrey King <jdking33@aol.com>
2016-12-10 09:25:27 -0800

    Databases: Add methods using where

--- models/address_book.rb
+++ models/address_book.rb
@@ -10,6 +10,14 @@ class AddressBook < KingRecord::Base
     Entry.create(name: name, phone_number: phone, email: email, address_book_id: 1)
   end
 
+  def entries
+    Entry.where(address_book_id: self.id)
+  end
+
+  def find_entry(name)
+    Entry.where(name: name, address_book_id: self.id).first
+  end
+
   def import_from_csv(file_name)
     # Implementation goes here
     csv_text = File.read(file_name)
